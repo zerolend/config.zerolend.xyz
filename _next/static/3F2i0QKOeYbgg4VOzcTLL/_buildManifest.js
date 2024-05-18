c1581d4c61ea1445c4270835ce6fd7b2278d029c
Deadshot Ryker <ryker@zerolend.xyz>
2024-05-18 20:08:54 +0400

    Updates

---
+++ _next/static/3F2i0QKOeYbgg4VOzcTLL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/176-dc2cc54e92a03b10.js","static/css/149b18973e5508c7.css","static/chunks/pages/index-e0407848c7691abc.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
