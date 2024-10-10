5b23a074516525f9dee23d077395333907fbae69
Deadshot Ryker <ryker@zerolend.xyz>
2024-10-10 17:39:35 +0400

    updae link

--- components/cells/LinkRenderer.tsx
+++ components/cells/LinkRenderer.tsx
@@ -1,24 +1,31 @@
 import { CustomCellRendererProps } from 'ag-grid-react';
 import React from 'react';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-
+import CheckIcon from '@mui/icons-material/Check';
 
 const LinkRenderer = (params: CustomCellRendererProps) => {
+    const [open, setOpen] = React.useState(false);
+
     const onClick = (e: React.MouseEvent) => {
         e.preventDefault();
         e.stopPropagation();
         navigator.clipboard.writeText(params.value.copy);
-        alert('Copied to clipboard');
+        setOpen(true);
+        setTimeout(() => setOpen(false), 1000)
     }
+
+    const Icon = open ? CheckIcon : ContentCopyIcon;
     return (
-        <span style={{ display: 'flex', fontFamily: params.value.copy ? 'monospace' : 'inherit', alignItems: 'center', justifyItems: 'center', color: 'blue', }}>
-            <a href={params.value.link} target='_blank' rel="noreferrer" style={{ color: 'blue', textDecoration: 'underline' }}>
-                {params.value.title}
-            </a>&nbsp;
-            {params.value.copy &&
-                (<ContentCopyIcon onClick={onClick} fontSize='small' style={{ cursor: 'pointer' }} color='inherit' />)
-            }
-        </span>
+        <>
+            <span style={{ display: 'flex', fontFamily: params.value.copy ? 'monospace' : 'inherit', alignItems: 'center', justifyItems: 'center', color: 'blue', }}>
+                <a href={params.value.link} target='_blank' rel="noreferrer" style={{ color: 'blue', textDecoration: 'underline' }}>
+                    {params.value.title}
+                </a>&nbsp;
+                {params.value.copy &&
+                    (<Icon onClick={onClick} fontSize='small' style={{ cursor: 'pointer' }} color='inherit' />)
+                }
+            </span>
+        </>
 
     )
 };
