946369033a0f5390e968a333e3846690065a4c45
Deadshot Ryker <151393124+deadshotryker@users.noreply.github.com>
2026-06-27 18:36:08 -0700

    Update marketconfig_zerolend.tsx

--- utils/marketconfig_zerolend.tsx
+++ utils/marketconfig_zerolend.tsx
@@ -59,9 +59,9 @@ export const marketConfigZerolend = {
   },
   zircuit: {
     chainId: 48900,
-    publicJsonRPCUrl: "https://zircuit1-mainnet.p2pify.com/",
+    publicJsonRPCUrl: "https://zircuit.zircuit.com/",
     LENDING_POOL_ADDRESS_PROVIDER: '0xFF679e5B4178A2f74A56f0e2c0e1FA1C80579385',
-    UI_POOL_DATA_PROVIDER: '0x189cfdb4d7a08D926CA209D84a713c4c629645aF',
+    UI_POOL_DATA_PROVIDER: '0x8053edF19352932F76Ea65FCEefbef9327B2619D',
     POOL: '0x2774C8B95CaB474D0d21943d83b9322Fb1cE9cF5',
     marketName: "proto_zircuit_v3",
   },
